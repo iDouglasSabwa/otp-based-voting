115e32831f8bdfec0d3ef615cb3e3777cd3e711a
Developer Douglas <dglsdeveloper@gmail.com>
2024-03-16 02:19:20 +0300

    Feat: Updated page logo and content to match new system

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ session_destroy();
 
     <head>
         <meta charset="utf-8" />
-        <title>Staff Awards and Recognition | Sabatia Eye Hospital</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">       <meta content="Sabatia Eye Hospital | Staff Awards and Recognition" name="Douglas Sabwa Indumwa" />
+        <title>Multi Factor Voting System</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">       <meta content="MultiFactor Voting System" name="Douglas Sabwa Indumwa" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
         <!-- App favicon -->
@@ -34,11 +34,11 @@ session_destroy();
                             <div class="card-body">
                                 <div class="px-3">
                                     <div class="auth-logo-box">
-                                        <a href="index.php" class="logo logo-admin"><img src="assets/images/logo-seh.png" height="55" alt="logo" class="auth-logo"></a>
+                                        <a href="index.php" class="logo logo-admin"><img src="assets/images/mflogo.png" height="55" alt="logo" class="auth-logo"></a>
                                     </div><!--end auth-logo-box-->
                                     
                                     <div class="text-center auth-logo-text">
-                                        <h4 class="mt-0 mb-1 mt-4 text-truncate" style="font-family: 'Poppins', sans-serif;line-height: 2.0;color: #0B59A4;"> SEH Staff Recognition</h4>
+                                        <h4 class="mt-0 mb-1 mt-4 text-truncate" style="font-family: 'Poppins', sans-serif;line-height: 2.0;color: #0B59A4;"> Multi Factor Voting System</h4>
                                        <!--  <p class="text-muted mb-0">Please sign in to continue</p>  -->
                                      
                                         <p class="text-muted mb-1" style="font-family: 'Poppins', sans-serif;"><?php echo " " . date("l, d M Y") . "<br>";;?></p>   
@@ -48,24 +48,16 @@ session_destroy();
                                     <form class="form-horizontal auth-form my-4" action="helpers/sindex.php" method="post" autocomplete="off">
             
                                         <div class="form-group">
-                                            <label for="staff_id" class="text-soft-dark"><b>Your Staff Number</b></label>
+                                            <label for="staff_id" class="text-soft-dark"><b>Voter ID Number</b></label>
                                             <div class="input-group mb-3">
                                                 <span class="auth-form-icon">
                                                     <i class="dripicons-user"></i> 
                                                 </span>
-                                                <input type="text" class="form-control" id="staff_id" name="staff_id" minlength="3" maxlength="5" placeholder="Staff Number here..." required >
+                                                <input type="text" class="form-control" id="staff_id" name="staff_id" minlength="3" maxlength="15" placeholder="Type here..." required >
                                             </div>                                    
                                         </div><!--end form-group--> 
             
-                                        <div class="form-group">
-                                            <label for="code" class="text-soft-dark"><b>Your Nomination Code</b></label>                                            
-                                            <div class="input-group mb-4"> 
-                                                <span class="auth-form-icon">
-                                                    <i class="dripicons-lock"></i> 
-                                                </span>                                                       
-                                                <input type="number" class="form-control" id="code" name="code" min="1000" max="9999" placeholder="Nomination Code here..." required>
-                                            </div>                               
-                                        </div><!--end form-group-->             
+                                                    
                                         
                                         <div class="form-group mb-0 row">
                                             <div class="col-12 mt-2">
@@ -82,7 +74,7 @@ session_destroy();
                             </div><!--end card-body-->
                         </div><!--end card-->
                         <div class="account-social text-center mt-4" style="visibility: visible;">
-                            <h6 class="my-4"> Sabatia Eye Hospital <script type="text/javascript"> document.write(new Date().getFullYear());</script></h6>
+                            <h6 class="my-4"> Tumutumu Girls <script type="text/javascript"> document.write(new Date().getFullYear());</script></h6>
                 
                         </div><!--end account-social-->
                     </div><!--end auth-page-->
