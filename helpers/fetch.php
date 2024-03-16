02da360712a294a4bfceb9ffe7b3aaea690a16c6
Developer Douglas <dglsdeveloper@gmail.com>
2024-03-16 07:00:57 +0300

    Feat: Fetch functions for all candidates in all political seats working

--- helpers/fetch.php
+++ helpers/fetch.php
@@ -1,42 +1,154 @@
 <?php
-//Fetch department list
-function department()
+//Fetch Presidential candidates list
+function president()
 {
 	// code...
 	include 'connect.php';
-	$sql = "SELECT id,section FROM sections ORDER BY section";
+	$sql = "SELECT voters.voter_name AS candidate,voters.id_number AS id_number, political_parties.party_name AS party_name FROM voters
+	INNER JOIN candidates ON voters.id_number = candidates.candidate 
+	INNER JOIN political_parties ON political_parties.party_id = candidates.political_party
+	WHERE candidates.political_seat = '1'
+	ORDER BY candidate";
 	$sql = mysqli_query($con,$sql);
 
 	foreach ($sql as $key => $value) {
 		// code...
-		$id = $value['id'];
-		$section = $value['section'];
+		$id_number = $value['id_number'];
+		$candidate = $value['candidate'];
+		$party_name = $value['party_name'];
 
-		echo '<option value="'.$id.'">'.$section.'</option>';
+		echo '<option value="'.$id_number.'">'.$candidate.' | '.$party_name.'</option>';
 
 	}
 
 	return mysqli_close($con);
 }
 
-//Fetch staff list
-function staff()
+//Fetch Governor candidates list
+function governor()
 {
 	// code...
 	include 'connect.php';
-	$sql = "SELECT pf_number,staff FROM staff ORDER BY staff";
+	$sql = "SELECT voters.voter_name AS candidate,voters.id_number AS id_number, political_parties.party_name AS party_name FROM voters
+	INNER JOIN candidates ON voters.id_number = candidates.candidate 
+	INNER JOIN political_parties ON political_parties.party_id = candidates.political_party
+	WHERE candidates.political_seat = '2'
+	ORDER BY candidate";
 	$sql = mysqli_query($con,$sql);
 
 	foreach ($sql as $key => $value) {
 		// code...
-		$pf_number = $value['pf_number'];
-		$staff = $value['staff'];
+		$id_number = $value['id_number'];
+		$candidate = $value['candidate'];
+		$party_name = $value['party_name'];
 
-		echo '<option value="'.$pf_number.'">'.$staff.'</option>';
+		echo '<option value="'.$id_number.'">'.$candidate.' | '.$party_name.'</option>';
 
 	}
 
 	return mysqli_close($con);
 }
+
+//Fetch Senatorial candidates list
+function senator()
+{
+	// code...
+	include 'connect.php';
+	$sql = "SELECT voters.voter_name AS candidate,voters.id_number AS id_number, political_parties.party_name AS party_name FROM voters
+	INNER JOIN candidates ON voters.id_number = candidates.candidate 
+	INNER JOIN political_parties ON political_parties.party_id = candidates.political_party
+	WHERE candidates.political_seat = '3'
+	ORDER BY candidate";
+	$sql = mysqli_query($con,$sql);
+
+	foreach ($sql as $key => $value) {
+		// code...
+		$id_number = $value['id_number'];
+		$candidate = $value['candidate'];
+		$party_name = $value['party_name'];
+
+		echo '<option value="'.$id_number.'">'.$candidate.' | '.$party_name.'</option>';
+
+	}
+
+	return mysqli_close($con);
+}
+
+//Fetch MP candidates list
+function MP()
+{
+	// code...
+	include 'connect.php';
+	$sql = "SELECT voters.voter_name AS candidate,voters.id_number AS id_number, political_parties.party_name AS party_name FROM voters
+	INNER JOIN candidates ON voters.id_number = candidates.candidate 
+	INNER JOIN political_parties ON political_parties.party_id = candidates.political_party
+	WHERE candidates.political_seat = '4'
+	ORDER BY candidate";
+	$sql = mysqli_query($con,$sql);
+
+	foreach ($sql as $key => $value) {
+		// code...
+		$id_number = $value['id_number'];
+		$candidate = $value['candidate'];
+		$party_name = $value['party_name'];
+
+		echo '<option value="'.$id_number.'">'.$candidate.' | '.$party_name.'</option>';
+
+	}
+
+	return mysqli_close($con);
+}
+
+//Fetch MCA candidates list
+function mca()
+{
+	// code...
+	include 'connect.php';
+	$sql = "SELECT voters.voter_name AS candidate,voters.id_number AS id_number, political_parties.party_name AS party_name FROM voters
+	INNER JOIN candidates ON voters.id_number = candidates.candidate 
+	INNER JOIN political_parties ON political_parties.party_id = candidates.political_party
+	WHERE candidates.political_seat = '5'
+	ORDER BY candidate";
+	$sql = mysqli_query($con,$sql);
+
+	foreach ($sql as $key => $value) {
+		// code...
+		$id_number = $value['id_number'];
+		$candidate = $value['candidate'];
+		$party_name = $value['party_name'];
+
+		echo '<option value="'.$id_number.'">'.$candidate.' | '.$party_name.'</option>';
+
+	}
+
+	return mysqli_close($con);
+}
+
+//Fetch Women Rep candidates list
+function womenrep()
+{
+	// code...
+	include 'connect.php';
+	$sql = "SELECT voters.voter_name AS candidate,voters.id_number AS id_number, political_parties.party_name AS party_name FROM voters
+	INNER JOIN candidates ON voters.id_number = candidates.candidate 
+	INNER JOIN political_parties ON political_parties.party_id = candidates.political_party
+	WHERE candidates.political_seat = '6'
+	ORDER BY candidate";
+	$sql = mysqli_query($con,$sql);
+
+	foreach ($sql as $key => $value) {
+		// code...
+		$id_number = $value['id_number'];
+		$candidate = $value['candidate'];
+		$party_name = $value['party_name'];
+
+		echo '<option value="'.$id_number.'">'.$candidate.' | '.$party_name.'</option>';
+
+	}
+
+	return mysqli_close($con);
+}
+
+
 
  ?>
